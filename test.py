e81808972d67b6716579d4f607517dece5054000
Gogi <garrybaga4anski@gmail.com>
2024-10-21 18:43:37 +0300

    first commit

---
+++ test.py
@@ -0,0 +1,17 @@
+class Vertex:
+    def __init__(this ,depth):
+        this.alpha = depth
+        pass
+    def __str__(this):
+        return this.alpha
+
+def renderScreenList(ScrList):
+    for i in range(len(ScrList)):
+        print("")
+        for k in range(len(ScrList[i])):
+            print(str(ScrList[i][k].alpha), end="")
+        
+
+
+scrList = [[Vertex(" ") for i in range(20)] for k in range(20)]
+renderScreenList(scrList)
